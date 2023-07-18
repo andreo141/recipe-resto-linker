9eff4e579f3b39d097242d7140a7e0981a4a1b94
Andreo Van Onckelen <andreo141mlg@gmail.com>
2023-07-18 15:27:19 +0200

    add backend support

--- frontend/app/routes/recipe.js
+++ frontend/app/routes/recipe.js
@@ -4,11 +4,10 @@ import fetch from 'fetch';
 export default class RecipeRoute extends Route {
   async model() {
     try {
-      const response = await fetch(
-        'https://www.themealdb.com/api/json/v1/1/random.php'
-      );
+      const response = await fetch('http://localhost:5000/random_meal');
       const data = await response.json();
-      return data.meals[0];
+      console.log(data);
+      return data;
     } catch (error) {
       console.error('Error:', error);
     }
