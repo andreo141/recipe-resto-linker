82fabf171ae653074257baa163e179f9e55348ec
Andreo Van Onckelen <andreo141mlg@gmail.com>
2023-07-24 11:34:12 +0200

    Remove Python backend

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -7,8 +7,8 @@
                 (:area :string, (s-prefix "schema:area"))
                 (:instructions :string, (s-prefix "schema:instructions"))
                 (:thumbnail :string, (s-prefix "schema:thumbnail"))
-                (:tags :string, (s-prefix "schema:tags"))
-  :resource-base (s-url "http://mu.semte.ch/application/")
+                (:tags :string, (s-prefix "schema:tags")))
+  :resource-base (s-url "http://mu.semte.ch/application/recipe-app")
   :on-path "recipes")
 
 (:dasherized-property-name :type, (s-prefix "my-prefix:my-predicate"))
