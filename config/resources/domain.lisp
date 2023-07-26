eb5d273260782b390593a403d004a9199bd25fb8
Andreo Van Onckelen <andreo141mlg@gmail.com>
2023-07-26 13:48:28 +0200

    changed 'creator' resource to 'restaurant'

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -4,21 +4,17 @@
   :class (s-prefix "schema:Recipe")
   :properties `((:name :string ,(s-prefix "schema:name"))
                 (:category :string , (s-prefix "schema:recipeCategory")))
-  :has-many `((creator :via , (s-prefix "schema:creator")
-                        :as "creators"))
+  :has-many `((restaurant :via , (s-prefix "schema:Restaurant")
+                        :as "restaurants"))
   :resource-base (s-url "http://mu.semte.ch/application/recipe-app/recipes/")
   :on-path "recipes")
 
-  (define-resource creator ()
-  :class (s-prefix "schema:Person")
-  :properties `((:creator :string ,(s-prefix "schema:creator"))
-                (:countryOfOrigin :string , (s-prefix "schema:countryOfOrigin")))
-  :has-many `((recipe :via , (s-prefix "schema:creator")
+(define-resource restaurant ()
+  :class (s-prefix "schema:Restaurant")
+  :properties `((:name :string ,(s-prefix "schema:name"))
+                (:description :string ,(s-prefix "schema:description"))
+  :has-many `((recipe :via , (s-prefix "schema:Restaurant")
                         :inverse t
-                        :as "recipes"))
-  :resource-base (s-url "http://mu.semte.ch/application/recipe-app/creators/")
-  :on-path "creators")
-
-
-
-
+                        :as "recipes")))
+  :resource-base (s-url "http://mu.semte.ch/application/recipe-app/restaurants/")
+  :on-path "restaurants")
